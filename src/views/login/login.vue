2869ec70cfbbd2c4da2534bcb860bd1141b923c3
洛克犀徐 <523402035@qq.com>
2019-12-13 19:31:01 +0800

    登录模块 - 标题区域布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="login-box">
-    <div class="form-box"></div>
+    <div class="form-box">
+      <div class="title-box">
+        <img src="../../assets/login-logo.png" alt />
+        <span class="title">黑马面面</span>
+        <i></i>
+        <span class="sub-title">用户登录</span>
+      </div>
+    </div>
     <img class="login-pic" src="../../assets/login_banner_ele.png" alt />
   </div>
 </template>
@@ -12,7 +19,9 @@ export default {};
 <style lang="less">
 .login-box {
   display: flex;
+  // 上下居中
   align-items: center;
+  // 左右均分
   justify-content: space-around;
   height: 100%;
   background: linear-gradient(
@@ -24,8 +33,41 @@ export default {};
   .form-box {
     width: 478px;
     height: 550px;
-    background: #F5F5F5;
-  };
+    background: #f5f5f5;
+    padding: 44px;
+    box-sizing: border-box;
+
+    .title-box {
+      display: flex;
+      align-items: center;
+
+      img {
+        // emmet语法
+        // w22+h17+mr16
+        width: 22px;
+        height: 17px;
+        margin-right: 16px;
+      }
+
+      .title {
+        font-size: 24px;
+        font-family: SourceHanSansCN;
+        margin-right: 14px;
+      }
+
+      i {
+        width: 1px;
+        height: 28px;
+        background-color: #c7c7c7;
+        margin-right: 12px;
+      }
+
+      .sub-title {
+        font-size: 22px;
+        font-family: PingFangSC;
+      }
+    }
+  }
 
   .login-pic {
   }
