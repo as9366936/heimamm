a1a131b7b870c84fe77ca0b89b630c7b52f22e3e
洛克犀徐 <523402035@qq.com>
2019-12-13 18:08:43 +0800

    登录模块 - 整体布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,10 +1,33 @@
 <template>
-    <div>登录模块</div>
+  <div class="login-box">
+    <div class="form-box"></div>
+    <img class="login-pic" src="../../assets/login_banner_ele.png" alt />
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="less">
+.login-box {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 100%;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+
+  .form-box {
+    width: 478px;
+    height: 550px;
+    background: #F5F5F5;
+  };
+
+  .login-pic {
+  }
+}
 </style>
