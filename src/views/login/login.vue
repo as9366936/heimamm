2ab9442a5f5d0c79e6153c5f4b4d7a4695082a22
洛克犀徐 <523402035@qq.com>
2019-12-15 15:30:06 +0800

    登录模块 - 整合表单验证

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -10,13 +10,13 @@
         <span class="sub-title">用户登录</span>
       </div>
       <!-- 饿了么的 表单 -->
-      <el-form class="login-form" ref="form" :model="form">
+      <el-form class="login-form" :rules="rules" ref="form" :model="form">
         <!-- 手机号 -->
-        <el-form-item>
-          <el-input placeholder="请输入手机号" prefix-icon="el-icon-user" v-model="form.input"></el-input>
+        <el-form-item prop="phone">
+          <el-input placeholder="请输入手机号" prefix-icon="el-icon-user" v-model="form.phone"></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input
             show-password
             placeholder="请输入密码"
@@ -25,7 +25,7 @@
           ></el-input>
         </el-form-item>
         <!-- 验证码 -->
-        <el-form-item>
+        <el-form-item prop="captcha">
           <!-- 行 -->
           <el-row>
             <!-- 列 -->
@@ -39,7 +39,7 @@
         </el-form-item>
         <!-- 用户协议 -->
         <el-form-item>
-          <el-checkbox v-model="form.type" class="checked-box">
+          <el-checkbox v-model="form.checked" class="checked-box">
             <div class="user-deal">
               我已阅读并同意
               <el-link type="primary" :underline="false">用户协议</el-link>和
@@ -49,8 +49,8 @@
         </el-form-item>
         <!-- 按钮区域 -->
         <el-form-item>
-          <el-button type="primary" class="login-btn">登录</el-button>
-          <el-button type="primary" class="register-btn">注册</el-button>
+          <el-button type="primary" class="login-btn" @click="submitForm">登录</el-button>
+          <el-button type="success" class="register-btn">注册</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -60,26 +60,84 @@
 
 <script>
 export default {
+  name: "login",
   data() {
+    /**
+     * @description: 自定义校验规则函数,检验手机号是否规范
+     * @param {type} rule 规则
+     * @param {string} value 用户输入的数据
+     * @param {function} callback 回调函数
+     * @return:
+     */
+    var checkPhone = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("手机号不能为空"));
+      } else {
+        // 判断手机号的格式
+        //正则
+        const reg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/;
+        // 判断是否符合
+        // .test(验证的字符串) 返回的是true 或者 false
+        if (reg.test(value) == true) {
+          callback();
+        } else {
+          // 不满足  手机号的格式
+          callback(new Error("老铁,你的手机号写错了"));
+        }
+      }
+    };
     return {
       form: {
-        input: "",
+        phone: "",
         password: "",
         captcha: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: ""
+        checked: false,
+      },
+      rules: {
+        // 手机号
+        phone: [{ required: true, validator: checkPhone, trigger: "blur" }],
+        // 密码
+        password: [
+          { required: true, message: "密码不能为空", trigger: "change" },
+          {
+            min: 6,
+            max: 18,
+            message: "密码长度为6 到 18",
+            trigger: "change"
+          }
+        ],
+        // 验证码
+        captcha: [
+          { required: true, message: "验证码不能为空", trigger: "blur" },
+          { min: 4, max: 4, message: "验证码长度为4", trigger: "change" }
+        ]
       }
     };
   },
   methods: {
-    onSubmit() {
-      window.console.log("submit!");
-    }
+    /**
+     * @description: 表单验证方法
+     * @param {type}
+     * @return:
+     */
+    submitForm() {
+      // 是否勾选用户协议
+      if (this.form.checked === false) {
+        this.$message.warning("请先勾选用户协议!");
+      } else {
+        this.$refs.form.validate(valid => {
+          if (valid) {
+            // 验证成功
+            this.$message.success("登录成功!");
+            // 调用接口
+          } else {
+            // 验证失败
+            this.$message.error("很遗憾,阁下有内容没有填对哦!");
+            return false;
+          }
+        });
+      }
+    },
   }
 };
 </script>
