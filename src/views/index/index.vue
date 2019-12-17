da685469dc5c276b69a635ea4960f840e8f97267
洛克犀徐 <523402035@qq.com>
2019-12-17 17:31:30 +0800

    首页模块 - 顶部布局

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,7 +1,19 @@
 <template>
   <!-- 布局容器 -->
   <el-container class="index-container">
-    <el-header class="my-header">Header</el-header>
+    <el-header class="my-header">
+      <div class="left">
+        <!-- 图标 -->
+        <i class="el-icon-s-fold icon"></i>
+        <img class="logo" src="../../assets/index_logo.png" alt="">
+        <span class="title">黑马面面</span>
+      </div>
+      <div class="right">
+        <img class="user-icon" src="../../assets/kuaijiu.jpg" alt="">
+        <span class="user-name">李达您好</span>
+        <el-button type="primary" size="small">退出</el-button>
+      </div>
+    </el-header>
     <el-container>
       <el-aside width="200px" class="my-aside">Aside</el-aside>
       <el-main class="my-main">Main</el-main>
@@ -19,7 +31,47 @@ export default {
 .index-container {
   height: 100%;
   .my-header{
-    background-color: red;
+    // background-color: red;
+    display: flex;
+    justify-content: space-between;
+
+    .left {
+      display: flex;
+      align-items: center;
+
+      .el-icon-s-fold {
+        font-size: 24px;
+        margin-right: 22px;
+      }
+
+      .logo {
+        width: 33px;
+        height: 28px;
+        margin-right: 11px;
+      }
+
+      .title {
+        font-size: 22px;
+        color: #49A1FF;
+      }
+    }
+
+    .right {
+      display: flex;
+      align-items: center;
+
+      .user-icon {
+        width: 43px;
+        height: 43px;
+        border-radius: 50%;
+        margin-right: 9px;
+      }
+
+      .user-name {
+        font-size: 14px;
+        margin-right: 38px;
+      }
+    }
   }
   .my-aside{
     background-color: green;
