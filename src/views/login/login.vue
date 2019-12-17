6c719aacfba9a6485e808d064cc49b675b75b43f
洛克犀徐 <523402035@qq.com>
2019-12-17 21:30:58 +0800

    登录验证模块 - 用户登录

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -285,7 +285,14 @@ export default {
               password: this.form.password,
               code: this.form.captcha
             }).then(res => {
-              window.console.log(res);
+              // window.console.log(res);
+              if (res.data.code === 202) {
+                this.$message.error(res.data.message + '!');
+              } else if (res.data.code === 200) {
+                localStorage.setItem("token", res.data.data.token);
+                this.$message.success("登录成功!");
+                this.$router.push("/index");
+              }
             });
           } else {
             // 验证失败
